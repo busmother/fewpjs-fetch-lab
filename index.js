afcb5a41cb577e20160994d026282a6c3eaf4d1b
Ian Marshall <imarshall14@gmail.com>
2021-03-24 21:31:21 -0400

    commit tree 593c57c72a249a4e9f1d263605888a19890c6782

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
+
+function fetchCharacters() {
+  return fetch("https://anapioficeandfire.com/api/characters")
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+}
+
+function renderCharacters(characters) {
+  const main = document.querySelector('main');
+  character = characters[1030];
+  const h3 = document.createElement('h3');
+  h3.innerHTML = character.name;
+  main.appendChild(h3);
+}
